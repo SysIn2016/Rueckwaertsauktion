1aad6f19e66579c463f70cdc9912e74a5eb3fe8c
kaulima19 <kaulima19@gmail.com>
2017-01-07 17:58:23 +0100

    Initiales Mavenprojekt

---
+++ src/main/java/Sysint2016/Rueckwaertsauktion/App.java
@@ -0,0 +1,13 @@
+package Sysint2016.Rueckwaertsauktion;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
