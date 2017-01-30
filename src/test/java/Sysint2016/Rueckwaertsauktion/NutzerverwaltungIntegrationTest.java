93619bed25144315360b2cd1a149edb057662426
mrep <Administrator@172.31.63.166>
2017-01-30 19:12:16 +0000

    Testphasen in pom.xml hinzugefuegt (diesmal wirklich)

--- src/test/java/Sysint2016/Rueckwaertsauktion/NutzerverwaltungIntegrationTest.java
+++ src/test/java/Sysint2016/Rueckwaertsauktion/NutzerverwaltungIntegrationTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class NutzerverwaltungTest {
+public class NutzerverwaltungIntegrationTest {
 	Nutzerverwaltung nv;
 	
 	@Before
