c4c38ef58de782e9fa5024e6d53da678e7acc154
kaulima19 <kaulima19@gmail.com>
2017-01-31 13:26:00 +0100

    Entfernung eines Tests, der AWS Credentials benötigt.

--- src/test/java/Sysint2016/Rueckwaertsauktion/NutzerverwaltungIntegrationTest.java
+++ src/test/java/Sysint2016/Rueckwaertsauktion/NutzerverwaltungIntegrationTest.java
@@ -26,18 +26,4 @@ public class NutzerverwaltungIntegrationTest {
 			fail("Test-Anmeldung fehlgeschlagen.");
 		}
 	}
-
-	/*
-	 * Registrierungsversuch mit bereits vorhandenem Nutzernamen
-	 * Erwartetes Ergebnis: Es erfolgt keine neue Registrierung
-	 */
-	@Test
-	public void testRegistrieren() {
-		try {
-			assertEquals(false, nv.registrieren("Peter", "Pan", "PeterPan", "email@adres.se", "passwort", "123", "123", "123"));
-		}
-		catch (Exception e) {
-			fail("Test-Registrierung fehlgeschlagen.");
-		}
-	}
 }
